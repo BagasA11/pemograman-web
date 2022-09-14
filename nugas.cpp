be82d4d566b668a48a383dbfbe880672c8468d48
BagasA11 <112991586+BagasA11@users.noreply.github.com>
2022-09-14 18:27:36 +0700

    Add files via upload

---
+++ nugas.cpp
@@ -0,0 +1,19 @@
+//judul
+
+//kamus
+//alas<<int
+//tinggi<<float
+//output<<bool
+//deskripsi
+#include<iostream>
+using namespace std;
+ main(){
+const float t = 1.5;
+int a;
+cin>>a;
+
+//jika a = 2t, maka segitiga siku-siku
+//jika a < 2t, maka segitiga lancip
+//jika a>2t, maka segitiga tumpul
+if(a == 2 * t){cout<<"segitiga siku2"<<endl;}else{cout<<"bukan siku2"<<endl;}
+ }
